ac37ef8b37fcc2236daf71ea98aaa066e0371ef0
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-05-12 11:17:01 -0700

    Porting functionality to scene load

--- src/SceneLoad.js
+++ src/SceneLoad.js
@@ -56,6 +56,134 @@ class SceneLoad extends Phaser.Scene{
         //starting camera follow
         scene.cameras.main.startFollow(scene.player);
 
+        /*
+            creating object groups
+        */
+        //creating invisible walls
+        scene.invisibleWallsGroup = scene.add.group({
+            classType: Phaser.Physics.Arcade.Sprite,
+            active: true,
+            runChildUpdate: false
+        }).addMultiple([
+            scene.physics.add.sprite(-50, config.height/2, 'invisible_wall').setImmovable().setVisible(false),
+            scene.physics.add.sprite(config.width+50, config.height/2, 'invisible_wall').setImmovable().setVisible(false),
+            scene.physics.add.sprite(config.width/2, 149, 'invisible_wall_rotated').setImmovable().setVisible(false)
+        ]);
+        
+        //creating the group to hold all the obstacles
+        scene.obstacleGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite.Obstacle,
+            defaultKey: null,               //default texture to use
+            defaultFrame: null,             //default animation frame start to use
+            active: true,
+            maxSize: 15,
+            runChildUpdate: true,
+            createCallback: null,           //what to do when an object is added to the group
+            removeCallback: null,           //what to do when an object is removed from the group
+            createMultipleCallback: null    //what to do when multiple objects are added to the group
+        });
+
+        //creating the group to hold all the enemies
+        scene.enemyGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite.Enemy,
+            active: true,
+            maxSize: 5,
+            runChildUpdate: true
+        });
+
+        //creating the group to hold all the attacks
+        scene.attackGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite,
+            active: true,
+            maxSize: -1,
+            runChildUpdate: true
+        });        
+
+        /*
+            creating spawning timers
+        */
+        //obstacle spawning timer
+        scene.obstacleSpawnTimer = scene.time.addEvent({
+            delay: 500,                // ms
+            callback: scene.createObstacle,
+            //args: [],
+            callbackScope: scene,
+            loop: true
+        });
+        
+        //enemy spawning timer
+        scene.enemySpawnTimer = scene.time.addEvent({
+            delay: 750,
+            callback: scene.createEnemy,
+            //args: [],
+            callbackScope: scene,
+            loop: true
+        });
+        
+        /*
+            creating colliders
+        */
+        scene.physics.add.collider(scene.player, scene.obstacleGroup, function(player){
+            player.startStun(250);
+        });
+        scene.physics.add.collider(scene.player, scene.enemyGroup, function(player, enemy){
+            enemy.onAttack(player);
+        });
+            //bUt tHOMAs scene Is AN oVeRlAP!!!1!111!! fuck outta here
+            //fuck outta here colliders are ineffective
+        scene.physics.add.overlap(scene.attackGroup, scene.enemyGroup, function(attack, enemy){
+            attack.strike(enemy);
+        })
+        scene.physics.add.overlap(scene.attackGroup, scene.obstacleGroup, function(attack, enemy){
+            //to simulate that it has struck nothing
+            attack.strike(null);
+        })
+        scene.physics.add.overlap(scene.attackGroup, scene.player, function(attack, player){
+            //to simulate that it has struck nothing
+            attack.strike(player);
+        })
+        //creating colliders for things that just need to collide
+        scene.physics.add.collider(scene.player, scene.invisibleWallsGroup);
+        scene.physics.add.collider(scene.enemyGroup, scene.obstacleGroup);
+        scene.physics.add.collider(scene.enemyGroup, scene.enemyGroup);
+        //scene.physics.add.collider(scene.enemyGroup, scene.invisibleWallsGroup);
+
+        /*
+            creating UI
+        */
+        scene.uiConfig = {
+            fontFamily: 'PermanentMarker',
+            fontSize: '28px',
+            //backgroundColor: '#F3B141',
+            color: '#6ABE30',
+            align: 'left',
+            stroke: '#000000',
+            strokeThickness: 10,
+            fixedWidth: 0
+        }
+        scene.meterText = scene.add.text(20, 20, "Meters: 0", scene.uiConfig).setScrollFactor(0);
+        scene.meterText.depth = 1;
+        scene.uiConfig.color = '#D62109';
+        //scene.uiConfig.align = 'right';
+        scene.scoreText = scene.add.text(20, 60, "Score: 0", scene.uiConfig,).setScrollFactor(0);
+        scene.scoreText.depth = 1;
+        scene.magicMissileMeter = [
+            scene.add.sprite(game.config.width - 40, 110, 'magic_missileUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 110, 110, 'magic_missileUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 180, 110, 'magic_missileUI').setScrollFactor(0)
+        ];
+        scene.magicMissileMeter[0].depth = 1;
+        scene.magicMissileMeter[1].depth = 1;
+        scene.magicMissileMeter[2].depth = 1;
+        scene.heartMeter = [
+            scene.add.sprite(game.config.width - 40, 40, 'heartUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 110, 40, 'heartUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 180, 40, 'heartUI').setScrollFactor(0)
+        ]
+        scene.heartMeter[0].depth = 1;
+        scene.heartMeter[1].depth = 1;
+        scene.heartMeter[2].depth = 1;
+
         //game-over flag
         scene.gameOver = false;
 
@@ -64,5 +192,131 @@ class SceneLoad extends Phaser.Scene{
         scene.killsUntilBoss = 15;    //should start at 15
         scene.bossActive = false;
         scene.boss;
+    }
+
+    //And now, a whole bunch of loading methods. Might just do this in a different file... nah...
+    defineGroups(){
+        //creating invisible walls
+        //don't forget to make these, ya know, actually invisible
+        scene.invisibleWallsGroup = scene.add.group({
+            classType: Phaser.Physics.Arcade.Sprite,
+            active: true,
+            runChildUpdate: false
+        }).addMultiple([
+            scene.physics.add.sprite(-50, config.height/2, 'invisible_wall').setImmovable().setVisible(false),
+            scene.physics.add.sprite(config.width+50, config.height/2, 'invisible_wall').setImmovable().setVisible(false),
+            scene.physics.add.sprite(config.width/2, 149, 'invisible_wall_rotated').setImmovable().setVisible(false)
+        ]);
+        
+        //creating the group to hold all the obstacles
+        scene.obstacleGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite.Obstacle,
+            defaultKey: null,               //default texture to use
+            defaultFrame: null,             //default animation frame start to use
+            active: true,
+            maxSize: 15,
+            runChildUpdate: true,
+            createCallback: null,           //what to do when an object is added to the group
+            removeCallback: null,           //what to do when an object is removed from the group
+            createMultipleCallback: null    //what to do when multiple objects are added to the group
+        });
+
+        //creating the group to hold all the enemies
+        scene.enemyGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite.Enemy,
+            active: true,
+            maxSize: 5,
+            runChildUpdate: true
+        });
+
+        //creating the group to hold all the attacks
+        scene.attackGroup = scene.add.group({
+            classType: Phaser.GameObjects.Sprite,
+            active: true,
+            maxSize: -1,
+            runChildUpdate: true
+        });        
+    }
+
+    defineSpawnTimers(){
+        //obstacle spawning timer
+        scene.obstacleSpawnTimer = scene.time.addEvent({
+            delay: 500,                // ms
+            callback: scene.createObstacle,
+            //args: [],
+            callbackScope: scene,
+            loop: true
+        });
+        
+        //enemy spawning timer
+        scene.enemySpawnTimer = scene.time.addEvent({
+            delay: 750,
+            callback: scene.createEnemy,
+            //args: [],
+            callbackScope: scene,
+            loop: true
+        });
+    }
+
+    defineColliders(){
+        scene.physics.add.collider(scene.player, scene.obstacleGroup, function(player){
+            player.startStun(250);
+        });
+        scene.physics.add.collider(scene.player, scene.enemyGroup, function(player, enemy){
+            enemy.onAttack(player);
+        });
+            //bUt tHOMAs scene Is AN oVeRlAP!!!1!111!! fuck outta here
+            //fuck outta here colliders are ineffective
+        scene.physics.add.overlap(scene.attackGroup, scene.enemyGroup, function(attack, enemy){
+            attack.strike(enemy);
+        })
+        scene.physics.add.overlap(scene.attackGroup, scene.obstacleGroup, function(attack, enemy){
+            //to simulate that it has struck nothing
+            attack.strike(null);
+        })
+        scene.physics.add.overlap(scene.attackGroup, scene.player, function(attack, player){
+            //to simulate that it has struck nothing
+            attack.strike(player);
+        })
+        //creating colliders for things that just need to collide
+        scene.physics.add.collider(scene.player, scene.invisibleWallsGroup);
+        scene.physics.add.collider(scene.enemyGroup, scene.obstacleGroup);
+        scene.physics.add.collider(scene.enemyGroup, scene.enemyGroup);
+        //scene.physics.add.collider(scene.enemyGroup, scene.invisibleWallsGroup);
+    }
+
+    defineUI(){
+        scene.uiConfig = {
+            fontFamily: 'PermanentMarker',
+            fontSize: '28px',
+            //backgroundColor: '#F3B141',
+            color: '#6ABE30',
+            align: 'left',
+            stroke: '#000000',
+            strokeThickness: 10,
+            fixedWidth: 0
+        }
+        scene.meterText = scene.add.text(20, 20, "Meters: 0", scene.uiConfig).setScrollFactor(0);
+        scene.meterText.depth = 1;
+        scene.uiConfig.color = '#D62109';
+        //scene.uiConfig.align = 'right';
+        scene.scoreText = scene.add.text(20, 60, "Score: 0", scene.uiConfig,).setScrollFactor(0);
+        scene.scoreText.depth = 1;
+        scene.magicMissileMeter = [
+            scene.add.sprite(game.config.width - 40, 110, 'magic_missileUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 110, 110, 'magic_missileUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 180, 110, 'magic_missileUI').setScrollFactor(0)
+        ];
+        scene.magicMissileMeter[0].depth = 1;
+        scene.magicMissileMeter[1].depth = 1;
+        scene.magicMissileMeter[2].depth = 1;
+        scene.heartMeter = [
+            scene.add.sprite(game.config.width - 40, 40, 'heartUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 110, 40, 'heartUI').setScrollFactor(0),
+            scene.add.sprite(game.config.width - 180, 40, 'heartUI').setScrollFactor(0)
+        ]
+        scene.heartMeter[0].depth = 1;
+        scene.heartMeter[1].depth = 1;
+        scene.heartMeter[2].depth = 1;
     }
 }
--- src/scenes/test/TestMap.js
+++ src/scenes/test/TestMap.js
@@ -4,7 +4,8 @@ class TestMap extends Phaser.Scene{
     }
 
     preload(){
-
+        //helpful array for obstacle generation
+        this.obstacleList = ['rock1', 'rock2', 'rock3', 'rock4', 'rock5', 'rock6'];
     }
 
     create(){
@@ -12,10 +13,88 @@ class TestMap extends Phaser.Scene{
     }
 
     update(){
+        //background scrolling
+        //this.background.tilePositionY -= 1;
+
         //game functionality
         if(!this.gameOver){
             //entity updating
             this.player.update();
+            this.textUpdate();
+
+            //check to spawn boss
+            if(!this.bossActive && this.killsUntilBoss <= this.player.bodyCount){
+                this.boss = new SkeletonKnightBoss(
+                    this,                                   //scene
+                    config.width/2,                         //x
+                    -325,                                    //y
+                    'enemies',                   //sprite
+                    'mid_attack1',                                      //start frame of anim
+                    this.bossLevel
+                    )
+                this.enemyGroup.add(this.boss);
+            }
         }
+    }
+
+    createObstacle(){
+        let obstacle = this.obstacleList[Phaser.Math.Between(0, this.obstacleList.length-1)];
+        if(!this.gameOver && !this.obstacleGroup.isFull()){
+            this.obstacleGroup.add(new Obstacle(
+                this,                                   //scene
+                Phaser.Math.Between(0, config.width),   //x
+                -32,                                    //y
+                'obstacles',                                //sprite
+                obstacle
+                )
+            );
+        }
+    }
+
+    createEnemy(){
+        if(!this.gameOver && !this.enemyGroup.isFull()){
+            this.enemyGroup.add(new Zombie(
+                this,                                   //scene
+                Phaser.Math.Between(0, config.width),    //x
+                -32,                                    //y
+                'enemies',                               //sprite
+                'forward_walk1',                                      //start frame of anim
+                )
+            );
+        }
+    }
+
+    textUpdate(){
+        this.meterText.setText("Meters: " + this.player.distance);
+        this.scoreText.setText("Score: " + this.player.score);
+    }
+
+    meterUpdate(param){
+        if(param == 0){
+            this.magicMissileMeter[0].visible = false;
+            this.magicMissileMeter[1].visible = false;
+            this.magicMissileMeter[2].visible = false;
+        }else if(param <= 3){
+            this.magicMissileMeter[param-1].visible = true;
+        }
+    }
+
+    healthUpdate(param){
+        this.heartMeter[param-1].visible = false;
+    }
+
+    finishGame(){
+        let scores = this.player.exportScores();
+        game.registry.set("score", scores[0]);
+        game.registry.set("distance", scores[1]);
+        game.registry.set("bodyCount", scores[2]);
+        this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+                this.scene.start("endScene");
+            },
+            callbackScope: this,
+            loop: false
+        });
     }
 }
